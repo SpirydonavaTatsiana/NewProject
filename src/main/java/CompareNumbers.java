bd0fc62d29b1a57288302af5b6ced6b830ff4c4b
Spirydonava Tatsiana <spirydonavatatsiana@gmail.com>
2024-07-27 12:42:56 +0300

    CompareNumbers

---
+++ src/main/java/CompareNumbers.java
@@ -0,0 +1,11 @@
+public class CompareNumbers {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
