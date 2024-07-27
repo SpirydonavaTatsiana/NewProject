78f1c22f8dd1f29290f647b3a3048281e20e9b95
Spirydonava Tatsiana <spirydonavatatsiana@gmail.com>
2024-07-27 12:53:23 +0300

    CreateAndMultipleArray

---
+++ src/main/java/CreateAndMultipleArray.java
@@ -0,0 +1,14 @@
+public class CreateAndMultipleArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] *= 2;
+            }
+        }
+        System.out.print("Массив с умноженными на 2 значениями: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
