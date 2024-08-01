e90c1082807f150e4e8e090ff57a842f748c876b
Spirydonava Tatsiana <spirydonavatatsiana@gmail.com>
2024-08-01 21:25:25 +0300

    post

--- src/test/java/ApiTestPostRaw.java
+++ src/test/java/ApiTestPostRaw.java
@@ -2,20 +2,21 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
-public class ApiTestGet {
+public class ApiTestPostRaw {
 
     @Test
-    public void testGetRequest() {
+    public void testPostRawRequest() {
         RestAssured.baseURI = "https://postman-echo.com";
 
         Response response = given()
-                .queryParam("foo1", "bar1")
-                .queryParam("foo2", "bar2")
+                .body("This is expected to be sent back as part of response body.")
                 .when()
-                .get("/get")
+                .post("/post")
                 .then()
                 .extract()
                 .response();
@@ -23,19 +24,23 @@ public class ApiTestGet {
         response.then().statusCode(200);
 
         response.then()
-                .body("args.foo1", equalTo("bar1"))
-                .body("args.foo2", equalTo("bar2"))
-                .body("headers.host", equalTo("postman-echo.com"))
-                .body("headers.x-request-start", startsWith("t="))
+                .body("args", equalTo(Collections.emptyMap()))
+                .body("data", equalTo("This is expected to be sent back as part of response body."))
+                .body("files", equalTo(Collections.emptyMap()))
+                .body("form", equalTo(Collections.emptyMap()))
+                .body("headers.x-forwarded-proto", equalTo("https"))
                 .body("headers.x-forwarded-port", equalTo("443"))
+                .body("headers.host", equalTo("postman-echo.com"))
                 .body("headers.x-amzn-trace-id", startsWith("Root="))
+                .body("headers.content-length", equalTo("58"))
+                .body("headers.content-type", startsWith("text/plain"))
                 .body("headers.user-agent", equalTo("Apache-HttpClient/4.5.13 (Java/11.0.21)"))
                 .body("headers.accept", equalTo("*/*"))
                 .body("headers.cache-control", equalTo(null))
                 .body("headers.postman-token", equalTo(null))
                 .body("headers.accept-encoding", equalTo("gzip,deflate"))
-                .body("headers.host", equalTo("postman-echo.com"))
                 .body("headers.cookie", equalTo(null))
-                .body("url", equalTo("https://postman-echo.com/get?foo1=bar1&foo2=bar2"));
+                .body("json", equalTo(null))
+                .body("url", equalTo("https://postman-echo.com/post"));
     }
 }
